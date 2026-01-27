79b32f306de9b78d9bb0c4cc0cf95770408db7a9
Lavoie Alexandre <alexlavoie90@hotmail.com>
2026-01-27 04:15:11 +0000

    File changes

--- src/pages/Chat_2.jsx
+++ src/pages/Chat_2.jsx
@@ -14,6 +14,7 @@ import ChatMessage from "../components/chat/ChatMessage";
 import { useConsciousnessHub } from "@/components/system/ConsciousnessHub";
 import ChatInput from "../components/chat/ChatInput";
 import ToolbarGenerators from "../components/chat/ToolbarGenerators";
+import InteractiveThought from "../components/chat/InteractiveThought";
 import ConsciousnessIndicator from "../components/chat/ConsciousnessIndicator";
 import { useLanguage } from "@/components/utils/LanguageContext";
 import { motion, AnimatePresence } from "framer-motion";
