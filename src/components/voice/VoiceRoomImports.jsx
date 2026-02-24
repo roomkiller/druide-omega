ecf947ac8c3df026daad9be9184ee3f8ce65b5fd
Lavoie Alexandre <alexlavoie90@hotmail.com>
2026-02-24 23:45:09 +0000

    File changes

---
+++ src/components/voice/VoiceRoomImports.jsx
@@ -0,0 +1,5 @@
+// Temporary imports file to satisfy VoiceRoom.jsx dependencies
+import VoiceRoomConnectionButton from '@/components/voice/VoiceRoomConnectionButton';
+import VoiceRoomSettingsPanel from '@/components/voice/VoiceRoomSettingsPanel';
+
+export { VoiceRoomConnectionButton, VoiceRoomSettingsPanel };
