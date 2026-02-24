5c0b10c882818dab9c7b9b3b727b411f2792f562
Lavoie Alexandre <alexlavoie90@hotmail.com>
2026-02-24 23:40:28 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -174,6 +174,7 @@ import VisualInteraction from './pages/VisualInteraction';
 import VoiceLive from './pages/VoiceLive';
 import VoiceRoom from './pages/VoiceRoom';
 import Workflows from './pages/Workflows';
+import VoiceRoomImports from './pages/VoiceRoomImports';
 import __Layout from './Layout.jsx';
 
 
@@ -305,6 +306,7 @@ export const PAGES = {
     "VoiceLive": VoiceLive,
     "VoiceRoom": VoiceRoom,
     "Workflows": Workflows,
+    "VoiceRoomImports": VoiceRoomImports,
 }
 
 export const pagesConfig = {
---
+++ src/pages/VoiceRoomImports.jsx
@@ -0,0 +1,2 @@
+export { default as VoiceRoomConnectionButton } from "@/components/voice/VoiceRoomConnectionButton";
+export { default as VoiceRoomSettingsPanel } from "@/components/voice/VoiceRoomSettingsPanel";
